20fc2572c23890f6ddb9ea33841c4d256ae4b9e4
AnastasiyaBurenkova <31570128+AnastasiyaBurenkova@users.noreply.github.com>
2017-11-26 14:16:57 +0400

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication18
+{
+    public partial class Form2 : Form
+
+    {
+        Parking parking;
+
+        public Form2()
+        {
+            InitializeComponent();
+            parking = new Parking();
+            Draw();
+        }
+        /// <summary>
+        /// Метод для отрисовки парковки
+        /// </summary>
+        private void Draw()
+        {
+            Bitmap bmp = new Bitmap(pictureBoxParking.Width, pictureBoxParking.Height);
+            Graphics gr = Graphics.FromImage(bmp);
+            parking.Draw(gr, pictureBoxParking.Width, pictureBoxParking.Height);
+            pictureBoxParking.Image = bmp;
+        }
+
+      
+
+        private void buttonSetCar_Click_1(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                var car = new Car(100, 4, 1000,500, dialog.Color);
+                int place = parking.PutCarInParking(car);
+                Draw();
+                MessageBox.Show("Ваше место: " + place);
+            }
+            
+        }
+
+        private void buttonSetGru_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    var car = new Gruzovik(100, 4, 1000,200, dialog.Color, true, true, true, dialogDop.Color);
+                    int place = parking.PutCarInParking(car);
+                    Draw();
+                    MessageBox.Show("Ваше место: " + place);
+                }
+            }
+        }
+
+        private void buttonTakeCar_Click_1(object sender, EventArgs e)
+        {
+            if (maskedTextBox1.Text != "")
+            {
+                var car = parking.GetCarInParking(Convert.ToInt32(maskedTextBox1.Text));
+
+                Bitmap bmp = new Bitmap(pictureBoxTakeCar.Width, pictureBoxTakeCar.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                car.setPosition(5, 5);
+                car.drawCar(gr);
+                pictureBoxTakeCar.Image = bmp;
+                Draw();
+            }
+        }
+    }
+}
